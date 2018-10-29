13ee7e781ce484ba9c9d883c6c84a210e8f04819
Alexander Thompson <legoboyjr@gmail.com>
2018-10-29 19:01:48 -0400

    green button works

--- app.js
+++ app.js
@@ -2,20 +2,25 @@
 const pegboard = document.querySelector('.pegboard');
 const resetBtn = document.querySelector('.resetBtn');
 const cells = document.querySelectorAll('.cell');
-
+const green = document.querySelector('.green');
+let selectedColor = "black";
 
 //events
 pegboard.addEventListener('click',onCellClick);
 resetBtn.addEventListener('click',reset);
+green.addEventListener('click',greenClick);
 
 //functions
 function onCellClick(event) {
     const element = event.target;
-    element.style.backgroundColor = "salmon";
+    element.style.backgroundColor = selectedColor;
 }
 function reset(event) {
     cells.forEach(function resetCell(cell){
         cell.style.backgroundColor = "";
     });
+}
+function greenClick(event) {
+    selectedColor = "green"
 }
 //kick it off
